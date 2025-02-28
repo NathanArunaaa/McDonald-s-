e20b9f3d244e21a2d8124940ba0f3e76fe4d9f7b
NathanArunaaa <nathanaruna07@gmail.com>
2025-02-28 18:55:43 -0500

    home screen added

--- McDonald's/ContentView.swift
+++ McDonald's/ContentView.swift
@@ -2,71 +2,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        TabView {
-            HomeView()
-                .tabItem {
-                    Label("Home", systemImage: "house.fill")
-                }
-
-            MenuView()
-                .tabItem {
-                    Label("Menu", systemImage: "menucard.fill")
-                }
-
-            OffersView()
-                .tabItem {
-                    Label("Offers", systemImage: "tag.fill")
-                }
-
-            ProfileView()
-                .tabItem {
-                    Label("Profile", systemImage: "person.fill")
-                }
-        }
-        .preferredColorScheme(.light) // Forces Light Mode
-
-    }
-}
-//asd
-
-struct HomeView: View {
-    var body: some View {
         NavigationStack {
             VStack {
                 NavigationLink(destination: DiscountView()) {
-                    Text("Go to Discount Page")
-                        .padding()
-                        .background(Color.red)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
+
+                Image("Home")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+
+                    
                 }
             }
         }
     }
 }
 
-
-
-struct MenuView: View {
-    var body: some View {
-        Text("Menu Screen")
-            .font(.title)
-    }
-}
-
-struct OffersView: View {
-    var body: some View {
-        Text("Offers Screen")
-            .font(.title)
-    }
-}
-
-struct ProfileView: View {
-    var body: some View {
-        Text("Profile Screen")
-            .font(.title)
-    }
-}
 
 #Preview {
     ContentView()
